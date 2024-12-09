e853985cf9e93209984b18346ed72f10c2078376
SZOU <sz3447@nyu.edu>
2024-12-09 22:47:55 +0800

    buttoin change

--- JS/intermediate.js
+++ JS/intermediate.js
@@ -1,3 +1,12 @@
-function redirectToNext() {
+let button;
+
+function setup(){
+    createCanvas(400,400);
+    canvas.parent("p5-canvas-container");
+    button = createButton("START");
+    button.mousePressed(redirect);
+}
+
+function redirect() {
     window.location.href = "main-game.html";
 }
